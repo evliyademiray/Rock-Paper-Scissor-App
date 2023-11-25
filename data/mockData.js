70c3bd808b29be980b6d166073dda6412b9b84af
Çiçek Demiray <cicekdemiray@Cicek-MacBook-Pro.local>
2023-11-25 22:00:01 +0300

    first commit

---
+++ data/mockData.js
@@ -0,0 +1,7 @@
+const choices =[
+    {name:'Taş',image:require('../assest/tas.png')},
+    {name:'Kağıt',image:require('../assest/kagit.png')},
+    {name:'Makas',image:require('../assest/makas.png')}
+];
+
+export default choices;
